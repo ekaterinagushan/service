9e3d4db6d165b9a0e7ac9e82bf7617abf9f1e58b
hse <gushan_katya@mail.ru>
2020-04-13 02:47:37 +0300

    Добавлены Ингредиенты и Состав, написаны связи

---
+++ app/models/ingredient.rb
@@ -0,0 +1,4 @@
+class Ingredient < ApplicationRecord
+  has_many: Structures
+  has_many: Product, thought: :Structures
+end
---
+++ app/models/structure.rb
@@ -0,0 +1,4 @@
+class Structure < ApplicationRecord
+  belongs_to: Product
+  belongs_to: Ingredient
+end
